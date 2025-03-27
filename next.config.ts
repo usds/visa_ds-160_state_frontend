a7e15191b1f46efcaa918ec09a9cd7d5366ed84e
Kristine <167804653+KristineSarnlertsophonUSDS@users.noreply.github.com>
2025-03-27 21:20:44 +0000

    update packages and next config to work on GH codespaces

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: "/site/[ownername]/[projectname]",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
