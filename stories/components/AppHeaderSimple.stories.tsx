321fc06a926469f53d7392017320e4cf38ece761
Kristine <167804653+KristineSarnlertsophonUSDS@users.noreply.github.com>
2025-04-01 17:39:01 +0000

    install storybook, add sample component and import styles

---
+++ stories/components/AppHeaderSimple.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import AppHeaderSimple from "../../components/AppHeaderSimple";
+
+//👇 This default export determines where your story goes in the story list
+const meta: Meta<typeof AppHeaderSimple> = {
+  component: AppHeaderSimple,
+};
+
+export default meta;
+type Story = StoryObj<typeof AppHeaderSimple>;
+
+export const FirstStory: Story = {
+  args: {
+    //👇 The args you need here will depend on your component
+  },
+};
