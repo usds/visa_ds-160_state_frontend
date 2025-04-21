9efec4093706e077972e5fc79a5da1cb2a6cc0de
Kristine <167804653+KristineSarnlertsophonUSDS@users.noreply.github.com>
2025-04-21 23:42:20 +0000

    storybook

--- components/UserForm/UserForm.stories.tsx
+++ components/UserForm/UserForm.stories.tsx
@@ -1,33 +1,33 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import UserForm from "./UserForm";
+import { withReactHookForm } from "../../.storybook/decorators";
 
 //👇 This default export determines where your story goes in the story list
 const meta: Meta<typeof UserForm> = {
   title: "Components/UserForm",
   component: UserForm,
+  decorators: [withReactHookForm],
 };
 
 export default meta;
 type Story = StoryObj<typeof UserForm>;
 
-export const Basic: Story = {
-  args: {
-    firstName: "Kay",
-    lastName: "Sarnasaurus",
-  },
-};
-export const Error: Story = {
-  args: {
-    firstName: "Kay",
-    lastName: "Sarnasaurus",
-    errorMessage: "Oops",
-  },
-};
+// export const Basic: Story = {
+//   args: {
+//     firstName: "Kay",
+//     lastName: "Sarnasaurus",
+//   },
+// };
+// export const Error: Story = {
+//   args: {
+//     firstName: "Kay",
+//     lastName: "Sarnasaurus",
+//     errorMessage: "Oops",
+//   },
+// };
 export const Success: Story = {
   args: {
-    firstName: "Kay",
-    lastName: "Sarnasaurus",
     successMessage: "Yay",
   },
 };
