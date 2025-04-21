da18d04dc816316aa5511256f576f3e99762e590
Kristine <167804653+KristineSarnlertsophonUSDS@users.noreply.github.com>
2025-04-21 23:13:12 +0000

    install react-hook-form, use for UserForm example

--- components/UserForm/UserForm.tsx
+++ components/UserForm/UserForm.tsx
@@ -1,36 +1,28 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Alert, Button, Form, Label, TextInput } from "@trussworks/react-uswds";
 
-type UserFormInputProps = {
+type UserFormInput = {
   firstName: string;
-  setFirstName: (event: string) => void;
   lastName: string;
-  setLastName: (event: string) => void;
-  errorMessage: string;
-  setErrorMessage: (event: string) => void;
+};
+
+type UserFormProps = {
   successMessage: string;
   setSuccessMessage: (event: string) => void;
 };
-const UserForm = (props: UserFormInputProps) => {
+
+const UserForm = (props: UserFormProps) => {
+  const { successMessage, setSuccessMessage } = props;
   const {
-    firstName,
-    setFirstName,
-    lastName,
-    setLastName,
-    errorMessage,
-    setErrorMessage,
-    successMessage,
-    setSuccessMessage,
-  } = props;
+    register,
+    handleSubmit,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useForm<UserFormInput>();
 
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-
-    const userData = {
-      first_name: firstName,
-      last_name: lastName,
-    };
-
+  const onSubmit: SubmitHandler<UserFormInput> = async (userData) => {
     try {
       const response = await fetch("http://localhost:8000/api/users", {
         method: "POST",
@@ -43,33 +35,38 @@ const UserForm = (props: UserFormInputProps) => {
       if (response.ok) {
         // Handle success
         setSuccessMessage("Form submitted successfully!");
-        setErrorMessage(null); // Clear any previous errors
+        clearErrors(); // Clear any previous errors
         console.log("User added successfully!");
       } else {
         // Handle error
         const errorData = await response.json();
-        setErrorMessage(
-          "There was an error submitting the form. Please try again.",
-        );
+        setError("root.serverError", {
+          type: response.statusText,
+          message:
+            "There was a server error submitting the form. Please try again.",
+        });
         setSuccessMessage(null); // Clear any previous success messages
         console.error("Error adding user.", errorData);
       }
     } catch (error) {
-      setErrorMessage(
-        "There was an error submitting the form. Please try again.",
-      );
+      setError("root.unknownError", {
+        type: "unknown",
+        message:
+          "There was an unknown error submitting the form. Please try again.",
+      });
       setSuccessMessage(null); // Clear any previous success messages
       console.error("Error adding user.", error);
     }
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      {errorMessage && (
-        <Alert headingLevel="h3" type="error">
-          {errorMessage}
-        </Alert>
-      )}
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      {errors.root &&
+        Object.keys(errors.root).map((errorName) => (
+          <Alert key={errorName} headingLevel="h3" type="error">
+            {errors.root[errorName].message}
+          </Alert>
+        ))}
       {successMessage && (
         <Alert headingLevel="h3" type="success">
           {successMessage}
@@ -77,21 +74,9 @@ const UserForm = (props: UserFormInputProps) => {
       )}
 
       <Label htmlFor="first-name">First Name</Label>
-      <TextInput
-        id="first-name"
-        name="first-name"
-        value={firstName}
-        onChange={(e) => setFirstName(e.target.value)}
-        required
-      />
+      <TextInput {...register("firstName")} required />
       <Label htmlFor="last-name">Last Name</Label>
-      <TextInput
-        id="last-name"
-        name="last-name"
-        value={lastName}
-        onChange={(e) => setLastName(e.target.value)}
-        required
-      />
+      <TextInput {...register("lastName")} required />
       <Button type="submit">Submit</Button>
     </Form>
   );
