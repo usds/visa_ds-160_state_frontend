19fd058bfd75d1ba10160ed789a67f8280ba7fa1
AlexYuleUSDS <142545767+AlexYuleUSDS@users.noreply.github.com>
2025-05-09 00:23:38 +0000

    passport details page

---
+++ src/app/passport-details/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
+import "../globals.css";
+import "@trussworks/react-uswds/lib/uswds.css";
+import "@trussworks/react-uswds/lib/index.css";
+import { Button, Fieldset, Form, Label, Select } from "@trussworks/react-uswds";
+import { useTranslations } from "next-intl";
+
+export default function PassportDetailsPage() {
+  const t = useTranslations("PassportDetails");
+  type detailsFormInput = {
+    surname: string;
+    givenName: string;
+    nativeAlphabetName: string;
+    nativeAlphabetDoesNotApply: boolean;
+  };
+  const formMethods = useForm<detailsFormInput>();
+
+  const onSubmit: SubmitHandler<detailsFormInput> = async (nameData) => {
+    console.log(nameData);
+  };
+
+  return (
+    <FormProvider {...formMethods}>
+      <h2>{t("header")}</h2>
+      <Form onSubmit={formMethods.handleSubmit(onSubmit)}>
+        <h3>{t("doc-type-header")}</h3>
+        <Fieldset legend={t("doc-type-hint")}>
+          {/* Passport Type */}
+          <Label htmlFor="doc-type-select" requiredMarker>
+            {t("doc-type-select-label")}
+          </Label>
+          <Select id="doc-type-select" name="doc-type-select">
+            <option>{t("regular-passport")}</option>
+          </Select>
+          <h3>{t("issuer-header")}</h3>
+          <Label htmlFor="issuer-select" requiredMarker>
+            {t("issuer-select-label")}
+          </Label>
+          <Select id="issuer-select" name="issuer-select">
+            <option>{t("issuer-option")}</option>
+          </Select>
+        </Fieldset>
+        <Button type="submit">{t("submit")}</Button>
+      </Form>
+    </FormProvider>
+  );
+}
