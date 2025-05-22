30c888ec799bf0b52f28cc61e88699b34c7891a7
AlexYuleUSDS <142545767+AlexYuleUSDS@users.noreply.github.com>
2025-05-22 11:56:33 -0700

    Names tokens (#20)
    * english tokens for names page
    
    * machine translations
    
    * Update messages/es.json
    
    Co-authored-by: Billy Griffin <william.n.griffin@omb.eop.gov>
    
    * Update messages/es.json
    
    Co-authored-by: Billy Griffin <william.n.griffin@omb.eop.gov>
    
    * Update messages/es.json
    
    Co-authored-by: Billy Griffin <william.n.griffin@omb.eop.gov>
    
    * Update messages/es.json
    
    Co-authored-by: Billy Griffin <william.n.griffin@omb.eop.gov>
    
    * Update messages/es.json
    
    Co-authored-by: Billy Griffin <william.n.griffin@omb.eop.gov>
    
    * Update messages/en.json
    
    Co-authored-by: Billy Griffin <william.n.griffin@omb.eop.gov>
    
    ---------
    
    Co-authored-by: AlexYuleUSDS <alexander.d.yule@usds.eop.gov>
    Co-authored-by: Billy Griffin <william.n.griffin@omb.eop.gov>

--- src/app/names/page.tsx
+++ src/app/names/page.tsx
@@ -14,8 +14,10 @@ import {
   Radio,
   TextInput,
 } from "@trussworks/react-uswds";
+import { useTranslations } from "next-intl";
 
 export default function NamePage() {
+  const t = useTranslations("Names");
   type nameFormInput = {
     surname: string;
     givenName: string;
@@ -30,76 +32,64 @@ export default function NamePage() {
 
   return (
     <FormProvider {...formMethods}>
-      <Alert
-        type="info"
-        heading="Answers to these questions should match what is written in your passport"
-        headingLevel="h4"
-      ></Alert>
+      <Alert type="info" heading={t("alert-header")} headingLevel="h4"></Alert>
       <h2>Names</h2>
       <Form onSubmit={formMethods.handleSubmit(onSubmit)}>
-        <Fieldset legend="Name on your current passport">
+        <Fieldset legend={t("current-name-header")}>
           {/* Surnames */}
           <Label htmlFor="surnames" requiredMarker>
-            Surnames (Last Names)
+            {t("surnames-label")}
           </Label>
-          <span className="usa-hint">
-            Enter all surnames (last names) as listed in your passport.
-          </span>
+          <span className="usa-hint">{t("surnames-hint")}</span>
           <TextInput id="surnames" required={true} />
 
           {/* Given names */}
           <Label htmlFor="givenNames" requiredMarker>
-            Given (First) Names
+            {t("given-names-label")}
           </Label>
-          <span className="usa-hint">
-            Enter your first names as listed in your passport.
-          </span>
+          <span className="usa-hint">{t("given-names-hint")}</span>
           <TextInput id="givenNames" required={true} />
 
           {/* Native Alphabet */}
           <Label htmlFor="nativeAlphabetName" requiredMarker>
-            Full Name in Native Alphabet
+            {t("native-alphabet-label")}
           </Label>
           <TextInput id="nativeAlphabetName" required={true} />
           <Checkbox
             id="nativeAlphabetDoesNotApply"
             name="nativeAlphabetDoesNotApply"
-            label="Does not apply"
+            label={t("does-not-apply")}
           />
         </Fieldset>
-        <Fieldset legend="Previous names">
+        <Fieldset legend={t("previous-names-header")}>
           {/* Previous names yes/no */}
-          <Fieldset legend="Have you ever used any other names?" requiredMarker>
-            <span className="usa-hint">
-              i.e. maiden name, religious, alias, etc
-            </span>
-            <Radio id="previousNames-yes" name="previousNames" label="yes" />
-            <Radio id="previousNames-no" name="previousNames" label="no" />
+          <Fieldset legend={t("previous-names-subheader")} requiredMarker>
+            <span className="usa-hint">{t("previous-names-hint")}</span>
+            <Radio
+              id="previousNames-yes"
+              name="previousNames"
+              label={t("yes")}
+            />
+            <Radio id="previousNames-no" name="previousNames" label={t("no")} />
           </Fieldset>
 
           {/* Previous names textboxes*/}
           <Label htmlFor="previous-name-1" requiredMarker>
-            Name 1
+            {t("name1")}
           </Label>
           <TextInput id="previous-name-1" required={true} />
           <Button type="button" outline>
-            + Add another name
+            {t("add-another-name")}
           </Button>
 
           {/* Telecode yes/no */}
-          <Fieldset
-            legend="Do you have a telecode that represents your name?"
-            requiredMarker
-          >
-            <span className="usa-hint">
-              A telecode is a 4 digit code that represents characters in some
-              non-Roman alphabet names
-            </span>
-            <Radio id="telecode-yes" name="telecode" label="yes" />
-            <Radio id="telecode-no" name="telecode" label="no" />
+          <Fieldset legend={t("telecode-header")} requiredMarker>
+            <span className="usa-hint">{t("telecode-hint")}</span>
+            <Radio id="telecode-yes" name="telecode" label={t("yes")} />
+            <Radio id="telecode-no" name="telecode" label={t("no")} />
           </Fieldset>
         </Fieldset>
-        <Button type="submit">Next</Button>
+        <Button type="submit">{t("submit")}</Button>
       </Form>
     </FormProvider>
   );
